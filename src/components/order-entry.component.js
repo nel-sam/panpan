040995e293158beded06e48ce1f3e378e03e9779
Nelson <nelson.rs.07@gmail.com>
2021-03-03 18:26:05 -0700

    Finished orderEntry logic

--- src/components/order-entry.component.js
+++ src/components/order-entry.component.js
@@ -1,19 +1,17 @@
-import React from "react";
+import React from 'react';
 import {
   Container,
   FormControl,
   FormLabel,
   FormGroup,
-  FormControlLabel,
-  Checkbox
-} from "@material-ui/core";
-import { connect } from "react-redux";
-import { setOrders} from '../state/actions/orderAction';
+  TextField,
+  Typography
+} from '@material-ui/core';
+import { connect } from 'react-redux';
+import { setOrders } from '../state/actions/orderAction';
 import { DoughTypes, LoafTypes } from '../enums';
 
 class OrderEntry extends React.Component {
-  orders = [];
-
   constructor(props) {
     super(props);
     this.state = {
@@ -21,29 +19,113 @@ class OrderEntry extends React.Component {
     }
   }
 
-  handleAddOrder = (event) => {
+  handleOrderChange = (doughType, loafType, count) => {
+    let newOrders = [];
+    const index = this.state.orders.findIndex(order =>
+      order.doughType === doughType && order.loafType === loafType);
+
+    if (index >= 0) {
+      newOrders = [...this.state.orders];
+debugger
+      if (count === 0) {
+        newOrders.splice(index, 1);
+      } else {
+        newOrders[index].count = count;
+      }
+    } else {
+      newOrders = [
+        ...this.state.orders, {
+          doughType: doughType,
+          loafType: loafType,
+          count: count,
+        }];
+    }
+
     this.setState({
+      orders: newOrders,
     });
 
-    this.props.setOrder(this.orders);
-  };
+    this.props.setOrders(newOrders);
+  }
+
+  handlePanOrderChange = (event) =>
+    this.handleOrderChange(event.target.id, LoafTypes.Pan, +event.target.value);
+  handleRoundOrderChange = (event) =>
+    this.handleOrderChange(event.target.id, LoafTypes.Round, +event.target.value);
 
   render() {
     return <Container>
-      <FormControl required component="fieldset">
-        <FormLabel component="legend">Dough Types</FormLabel>
-        <FormGroup row>
-          <FormControlLabel
-            control={<Checkbox checked={this.state.sourdough} onChange={this.handleDoughToggle} name={DoughTypes.Sourdough} />}
-            label="Sour Dough" />
-          <FormControlLabel
-            control={<Checkbox checked={this.state.banana} onChange={this.handleDoughToggle} name={DoughTypes.Banana} />}
-            label="Banana" />
-          <FormControlLabel
-            control={<Checkbox checked={this.state.wholeGrain} onChange={this.handleDoughToggle} name={DoughTypes.WholeGrain} />}
-            label="Whole Grain" />
-        </FormGroup>
+      <FormControl component='fieldset' style={{ marginTop: 30 }}>
+        <Container>
+          <FormLabel component='legend'>Pan Loaf Orders</FormLabel>
+          <FormGroup>
+            <TextField
+              id={DoughTypes.Sourdough}
+              label='Sour Dough'
+              type='number'
+              InputLabelProps={{ shrink: true }}
+              InputProps={{ inputProps: { min: 0 } }}
+              variant='outlined'
+              margin='normal'
+              onChange={this.handlePanOrderChange} />
+            <TextField
+              id={DoughTypes.Banana}
+              label='Banana'
+              type='number'
+              InputLabelProps={{ shrink: true }}
+              InputProps={{ inputProps: { min: 0 } }}
+              variant='outlined'
+              margin='normal'
+              onChange={this.handlePanOrderChange} />
+            <TextField
+              id={DoughTypes.WholeGrain}
+              label='Whole Grain'
+              type='number'
+              InputLabelProps={{ shrink: true }}
+              InputProps={{ inputProps: { min: 0 } }}
+              variant='outlined'
+              margin='normal'
+              onChange={this.handlePanOrderChange} />
+          </FormGroup>
+        </Container>
+        <Container>
+          <FormLabel component='legend'>Round Loaf Orders</FormLabel>
+          <FormGroup>
+            <TextField
+              id={DoughTypes.Sourdough}
+              label='Sour Dough'
+              type='number'
+              InputLabelProps={{ shrink: true }}
+              InputProps={{ inputProps: { min: 0 } }}
+              variant='outlined'
+              margin='normal'
+              onChange={this.handleRoundOrderChange} />
+            <TextField
+              id={DoughTypes.Banana}
+              label='Banana'
+              type='number'
+              InputLabelProps={{ shrink: true }}
+              InputProps={{ inputProps: { min: 0 } }}
+              variant='outlined'
+              margin='normal'
+              onChange={this.handleRoundOrderChange} />
+            <TextField
+              id={DoughTypes.WholeGrain}
+              label='Whole Grain'
+              type='number'
+              InputLabelProps={{ shrink: true }}
+              InputProps={{ inputProps: { min: 0 } }}
+              variant='outlined'
+              margin='normal'
+              onChange={this.handleRoundOrderChange} />
+          </FormGroup>
+        </Container>
       </FormControl>
+      <Container>
+        {this.state.orders.length > 0 && <Typography>Current order</Typography>}
+        {this.state.orders.map(order =>
+          <Typography>{order.doughType} {order.loafType}: {order.count}</Typography>) }
+      </Container>
     </Container>;
   }
 }
--- src/components/plan-display.component.js
+++ src/components/plan-display.component.js
@@ -2,19 +2,20 @@ import React from 'react';
 import { Container, Typography } from "@material-ui/core";
 import { useSelector } from 'react-redux'
 
-const useDoughs= () => {
+const useStateObjs= () => {
   return useSelector(
     (state) => ({
       doughs: state.doughs,
+      orders: state.orders,
     })
   );
 }
 
 const PlanDisplay = () => {
-  const { doughs } = useDoughs();
+  const { doughs, orders } = useStateObjs();
 
   return <Container>
-    <Typography>Plans {doughs.length}</Typography>
+    <Typography>Plans {doughs.length} {orders.length}</Typography>
   </Container>;
 }
 
